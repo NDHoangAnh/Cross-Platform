4f409b6fc0d5249974a1fe794a645c1b05a1d9b8
sucrub <bachspham@gmail.com>
2023-11-07 17:23:36 +0700

    Initial login screen and re-build App.tsx for navigate

--- App.tsx
+++ App.tsx
@@ -1,17 +1,31 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from './src/scenes/Home';
 import Settings from './src/scenes/Settings';
+import Login from './src/scenes/Login';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Settings" component={Settings} />
-      </Tab.Navigator>
+      {isLoggedIn ? (
+        <Tab.Navigator screenOptions={{headerShown: false}}>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
---
+++ src/scenes/Login/index.style.ts
@@ -0,0 +1,67 @@
+/* eslint-disable prettier/prettier */
+
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    color: 'black',
+    marginTop: '30%',
+    marginLeft: '10%',
+  },
+  note: {
+    fontSize: 18,
+    color: 'gray',
+    marginLeft: '10%',
+  },
+  textbox: {
+    marginTop: '25%',
+    alignItems: 'center',
+  },
+  input: {
+    color: 'black',
+    height: 40,
+    width: '80%',
+    margin: 12,
+    borderWidth: 0,
+    padding: 10,
+    marginBottom: '5%',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.4,
+    shadowRadius: 5,
+    elevation: 5,
+    backgroundColor: 'white',
+  },
+  button: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+    width: '30%',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  register: {
+    marginTop: '5%',
+    color: 'gray',
+  },
+  link: {
+    textDecorationLine: 'underline',
+    color: 'blue',
+  },
+});
+
+export default styles;
+
