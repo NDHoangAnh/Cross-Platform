6dd0ae4d9abfe439ac31664d66b39ec0ad441a26
NDHoangAnh <hoanganh.axe.no.1@gmail.com>
2023-12-25 12:00:46 +0700

    Fix

---
+++ src/data/post.js
@@ -0,0 +1,230 @@
+export const posts = [
+  {
+    id: 1,
+    user: 'Test',
+    avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+    createdAt: '10/11/2023',
+    image: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+    like: 5,
+    comments: [
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+    ],
+  },
+  {
+    id: 2,
+    user: 'Test',
+    avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+    createdAt: '10/11/2023',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+    like: 5,
+    comments: [
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+    ],
+  },
+  {
+    id: 3,
+    user: 'Test',
+    avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+    createdAt: '10/11/2023',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+    like: 5,
+    comments: [
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+    ],
+  },
+  {
+    id: 4,
+    user: 'Test',
+    avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+    createdAt: '10/11/2023',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+    like: 5,
+    comments: [
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+    ],
+  },
+  {
+    id: 5,
+    user: 'Test',
+    avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+    createdAt: '10/11/2023',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+    like: 5,
+    comments: [
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+    ],
+  },
+  {
+    id: 6,
+    user: 'Test',
+    avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+    createdAt: '10/11/2023',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+    like: 5,
+    comments: [
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+      {
+        user: 'Test cmt',
+        createdAt: '10/11/2023',
+        avatar: 'https://avatars.githubusercontent.com/u/74105921?v=4',
+        content:
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure impedit animi quam. Eum, saepe quasi itaque assumenda doloremque veritatis nam earum aperiam enim facere. Quae quisquam error et? Officia, qui.',
+      },
+    ],
+  },
+];
