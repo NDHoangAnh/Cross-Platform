9907690d3e06a62570b6dc468dc8e021829706c6
Thanh Do <thanh.do@jmango360.com>
2024-01-07 22:48:57 +0700

    fix bug toast message

--- src/scenes/Target/CreateTarget.tsx
+++ src/scenes/Target/CreateTarget.tsx
@@ -14,6 +14,7 @@ import {useState} from 'react';
 import {useForm, Controller} from 'react-hook-form';
 import apis from '../../apis';
 import asyncData from '../../config/auth';
+import Toast from "react-native-toast-message";
 
 const CreateTarget = ({navigation}) => {
   const {
@@ -50,126 +51,134 @@ const CreateTarget = ({navigation}) => {
     if (data) {
       setLoadingButtonSave(false);
       navigation.navigate('Target', {screen: 'TargetScreen'});
+      Toast.show({
+        type: 'success',
+        text1: 'Create target',
+        text2: 'Create success',
+      });
     }
     reset();
   };
 
   return (
-    <ScrollView style={styles.container} stickyHeaderIndices={[0]}>
-      <Navbar listAction={[]} />
-      <View style={styles.formCreateTargetContainer}>
-        <Text style={{fontWeight: '600', fontSize: 24, color: 'black'}}>
-          Create Target
-        </Text>
-        <View style={styles.inputContainer}>
-          <View style={styles.formItem}>
-            <Text style={styles.formLabel}>Name</Text>
-            <View style={{flex: 1}}>
-              <Controller
-                control={control}
-                rules={{
-                  required: true,
-                }}
-                render={({field: {onChange, onBlur, value}}) => (
-                  <TextInput
-                    placeholder="Name"
-                    onBlur={onBlur}
-                    onChangeText={onChange}
-                    value={value}
-                    style={styles.input}
+      <>
+        <ScrollView style={styles.container} stickyHeaderIndices={[0]}>
+          <Navbar listAction={[]} />
+          <View style={styles.formCreateTargetContainer}>
+            <Text style={{fontWeight: '600', fontSize: 24, color: 'black'}}>
+              Create Target
+            </Text>
+            <View style={styles.inputContainer}>
+              <View style={styles.formItem}>
+                <Text style={styles.formLabel}>Name</Text>
+                <View style={{flex: 1}}>
+                  <Controller
+                      control={control}
+                      rules={{
+                        required: true,
+                      }}
+                      render={({field: {onChange, onBlur, value}}) => (
+                          <TextInput
+                              placeholder="Name"
+                              onBlur={onBlur}
+                              onChangeText={onChange}
+                              value={value}
+                              style={styles.input}
+                          />
+                      )}
+                      name="name"
                   />
-                )}
-                name="name"
-              />
+                </View>
+              </View>
+              {errors.name && (
+                  <Text style={styles.errorText}>This is required.</Text>
+              )}
+              <View style={styles.formItem}>
+                <Text style={styles.formLabel}>Description</Text>
+                <View style={{flex: 1}}>
+                  <Controller
+                      control={control}
+                      rules={{
+                        required: true,
+                      }}
+                      render={({field: {onChange, onBlur, value}}) => (
+                          <TextInput
+                              placeholder="Description"
+                              onBlur={onBlur}
+                              onChangeText={onChange}
+                              value={value}
+                              style={styles.input}
+                          />
+                      )}
+                      name="description"
+                  />
+                </View>
+              </View>
+              {errors.name && (
+                  <Text style={styles.errorText}>This is required.</Text>
+              )}
+              <View style={styles.formItem}>
+                <Text style={styles.formLabel}>Real Point</Text>
+                <View style={{flex: 1}}>
+                  <Controller
+                      control={control}
+                      rules={{
+                        required: true,
+                      }}
+                      render={({field: {onChange, onBlur, value}}) => (
+                          <TextInput
+                              placeholder="0"
+                              onBlur={onBlur}
+                              onChangeText={onChange}
+                              value={value}
+                              style={styles.input}
+                          />
+                      )}
+                      name="realPoint"
+                  />
+                </View>
+              </View>
+              {errors.name && (
+                  <Text style={styles.errorText}>This is required.</Text>
+              )}
+              <View style={styles.formItem}>
+                <Text style={styles.formLabel}>Target Point</Text>
+                <View style={{flex: 1}}>
+                  <Controller
+                      control={control}
+                      rules={{
+                        required: true,
+                      }}
+                      render={({field: {onChange, onBlur, value}}) => (
+                          <TextInput
+                              placeholder="0"
+                              onBlur={onBlur}
+                              onChangeText={onChange}
+                              value={value}
+                              style={styles.input}
+                          />
+                      )}
+                      name="targetPoint"
+                  />
+                </View>
+              </View>
+              {errors.name && (
+                  <Text style={styles.errorText}>This is required.</Text>
+              )}
+            </View>
+            <View>
+              <TouchableOpacity
+                  disabled={loadingButtonSave}
+                  style={styles.button}
+                  onPress={handleSubmit(onSubmit)}>
+                <Text style={styles.textBtn}>Create</Text>
+              </TouchableOpacity>
+              <ActivityIndicator animating={loadingButtonSave} />
             </View>
           </View>
-          {errors.name && (
-            <Text style={styles.errorText}>This is required.</Text>
-          )}
-          <View style={styles.formItem}>
-            <Text style={styles.formLabel}>Description</Text>
-            <View style={{flex: 1}}>
-              <Controller
-                control={control}
-                rules={{
-                  required: true,
-                }}
-                render={({field: {onChange, onBlur, value}}) => (
-                  <TextInput
-                    placeholder="Description"
-                    onBlur={onBlur}
-                    onChangeText={onChange}
-                    value={value}
-                    style={styles.input}
-                  />
-                )}
-                name="description"
-              />
-            </View>
-          </View>
-          {errors.name && (
-            <Text style={styles.errorText}>This is required.</Text>
-          )}
-          <View style={styles.formItem}>
-            <Text style={styles.formLabel}>Real Point</Text>
-            <View style={{flex: 1}}>
-              <Controller
-                control={control}
-                rules={{
-                  required: true,
-                }}
-                render={({field: {onChange, onBlur, value}}) => (
-                  <TextInput
-                    placeholder="0"
-                    onBlur={onBlur}
-                    onChangeText={onChange}
-                    value={value}
-                    style={styles.input}
-                  />
-                )}
-                name="realPoint"
-              />
-            </View>
-          </View>
-          {errors.name && (
-            <Text style={styles.errorText}>This is required.</Text>
-          )}
-          <View style={styles.formItem}>
-            <Text style={styles.formLabel}>Target Point</Text>
-            <View style={{flex: 1}}>
-              <Controller
-                control={control}
-                rules={{
-                  required: true,
-                }}
-                render={({field: {onChange, onBlur, value}}) => (
-                  <TextInput
-                    placeholder="0"
-                    onBlur={onBlur}
-                    onChangeText={onChange}
-                    value={value}
-                    style={styles.input}
-                  />
-                )}
-                name="targetPoint"
-              />
-            </View>
-          </View>
-          {errors.name && (
-            <Text style={styles.errorText}>This is required.</Text>
-          )}
-        </View>
-        <View>
-          <TouchableOpacity
-            disabled={loadingButtonSave}
-            style={styles.button}
-            onPress={handleSubmit(onSubmit)}>
-            <Text style={styles.textBtn}>Create</Text>
-          </TouchableOpacity>
-          <ActivityIndicator animating={loadingButtonSave} />
-        </View>
-      </View>
-    </ScrollView>
+        </ScrollView>
+        <Toast/>
+      </>
   );
 };
 
